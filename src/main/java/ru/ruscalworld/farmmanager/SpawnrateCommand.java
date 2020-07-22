8607986c88f4c3863a192d9176044afbe8e50658
Nikolay <gladyschevnl@gmail.com>
2020-07-22 18:46:20 +0300

    Добавлено отображение лимита water ambient

--- src/main/java/ru/ruscalworld/farmmanager/SpawnrateCommand.java
+++ src/main/java/ru/ruscalworld/farmmanager/SpawnrateCommand.java
@@ -15,12 +15,14 @@ public class SpawnrateCommand implements CommandExecutor {
             int animals = Bukkit.getAnimalSpawnLimit();
             int water_animals = Bukkit.getWaterAnimalSpawnLimit();
             int ambient = Bukkit.getAmbientSpawnLimit();
+            int water_ambient = Bukkit.getServer().getWaterAmbientSpawnLimit();
 
             commandSender.sendMessage("§f§lТекущие параметры спавна мобов: \n" +
                     "§7§l * §fМонстры: §9" + monsters + "\n" +
                     "§7§l * §fЖивотные: §9" + animals + "\n" +
                     "§7§l * §fВодные животные: §9" + water_animals + "\n" +
-                    "§7§l * §fAmbient: §9" + ambient);
+                    "§7§l * §fAmbient: §9" + ambient + "\n" +
+                    "§7§l * §fWater Ambient: §9" + water_ambient);
         }
 
         return false;
