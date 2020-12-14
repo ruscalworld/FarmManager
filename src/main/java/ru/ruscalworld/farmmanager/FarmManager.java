a1d5a631a76a861f94399f62a55f073078c9a213
RuscalWorld <me@ruscalworld.ru>
2020-12-14 11:40:11 +0300

    Рефакторинг, обновлённые настройки

--- src/main/java/ru/ruscalworld/farmmanager/FarmManager.java
+++ src/main/java/ru/ruscalworld/farmmanager/FarmManager.java
@@ -1,12 +1,8 @@
 package ru.ruscalworld.farmmanager;
 
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Calendar;
+import java.sql.Timestamp;
 import java.util.Objects;
 
 public class FarmManager extends JavaPlugin {
@@ -32,51 +28,8 @@ public class FarmManager extends JavaPlugin {
         getConfig().set("multiplier", multiplier);
         saveConfig();
 
-        Calendar cal = Calendar.getInstance();
-
-        int monster_limit;
-        int animal_limit;
-        int water_animal_limit;
-        int ambient_limit;
-        int water_ambient_limit;
-
-        cal.setTimeInMillis(System.currentTimeMillis());
-        int day = cal.get(Calendar.DAY_OF_WEEK);
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-
-        monster_limit = 7;
-        animal_limit = 2;
-        water_animal_limit = 1;
-        ambient_limit = 1;
-        water_ambient_limit = 1;
-
-        if (hour == 0) {
-            monster_limit = 70;
-            animal_limit = 15;
-            water_animal_limit = 5;
-            ambient_limit = 15;
-            water_ambient_limit = 20;
-        }
-
-        File configFile = new File(getDataFolder() + "/../..", "bukkit.yml");
-        YamlConfiguration config = new YamlConfiguration();
-
-        try {
-            config.load(configFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-
-        config.set("spawn-limits.monsters", monster_limit);
-        config.set("spawn-limits.animals", animal_limit);
-        config.set("spawn-limits.water-animals", water_animal_limit);
-        config.set("spawn-limits.ambient", ambient_limit);
-        config.set("spawn-limits.water-ambient", water_ambient_limit);
-
-        try {
-            config.save(configFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Spawnrate spawnrate = Spawnrate.getByTime(timestamp);
+        spawnrate.apply();
     }
 }
---
+++ src/main/java/ru/ruscalworld/farmmanager/Spawnrate.java
@@ -0,0 +1,99 @@
+package ru.ruscalworld.farmmanager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+public class Spawnrate {
+    private final int monsterLimit;
+    private final int animalLimit;
+    private final int waterAnimalLimit;
+    private final int ambientLimit;
+    private final int waterAmbientLimit;
+
+    public Spawnrate(int monsterLimit, int animalLimit, int waterAnimalLimit, int ambientLimit, int waterAmbientLimit) {
+        this.monsterLimit = monsterLimit;
+        this.animalLimit = animalLimit;
+        this.waterAnimalLimit = waterAnimalLimit;
+        this.ambientLimit = ambientLimit;
+        this.waterAmbientLimit = waterAmbientLimit;
+    }
+
+    public Spawnrate() {
+        this.monsterLimit = Bukkit.getMonsterSpawnLimit();
+        this.animalLimit = Bukkit.getAnimalSpawnLimit();
+        this.waterAnimalLimit = Bukkit.getWaterAnimalSpawnLimit();
+        this.ambientLimit = Bukkit.getAmbientSpawnLimit();
+        this.waterAmbientLimit = Bukkit.getWaterAmbientSpawnLimit();
+    }
+
+    public static Spawnrate getByTime(Timestamp time) {
+        Calendar cal = Calendar.getInstance();
+
+        int monsterLimit;
+        int animalLimit;
+        int waterAnimalLimit;
+        int ambientLimit;
+        int waterAmbientLimit;
+
+        cal.setTimeInMillis(time.getTime());
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+
+        // Лимиты по умолчанию (во всё остальное время)
+        monsterLimit = 10;
+        animalLimit = 10;
+        waterAnimalLimit = 3;
+        ambientLimit = 5;
+        waterAmbientLimit = 5;
+
+        // Только в 10 часов с понедельника по четверг
+        if (hour == 10 && (day == Calendar.MONDAY || day == Calendar.TUESDAY || day == Calendar.WEDNESDAY || day == Calendar.THURSDAY)) {
+            monsterLimit = 35;
+            animalLimit = 35;
+            waterAnimalLimit = 5;
+            ambientLimit = 7;
+            waterAmbientLimit = 8;
+        }
+
+        // Ночью всегда стандартные значения
+        if (hour == 0) {
+            monsterLimit = 70;
+            animalLimit = 15;
+            waterAnimalLimit = 5;
+            ambientLimit = 15;
+            waterAmbientLimit = 20;
+        }
+
+        return new Spawnrate(monsterLimit, animalLimit, waterAnimalLimit, ambientLimit, waterAmbientLimit);
+    }
+
+    public void apply() {
+        String path = Bukkit.getWorldContainer().getAbsolutePath();
+        File configFile = new File(path, "bukkit.yml");
+        YamlConfiguration config = new YamlConfiguration();
+
+        try {
+            config.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        config.set("spawn-limits.monsters", this.monsterLimit);
+        config.set("spawn-limits.animals", this.animalLimit);
+        config.set("spawn-limits.water-animals", this.waterAnimalLimit);
+        config.set("spawn-limits.ambient", this.ambientLimit);
+        config.set("spawn-limits.water-ambient", this.waterAmbientLimit);
+
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
